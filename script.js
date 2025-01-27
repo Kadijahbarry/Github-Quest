f3b9f8a10bcabcd81d9775a5b2e5995f4d1599cb
Kadijah Barry <Kadijahbarry256@gmail.com>
2025-01-27 00:31:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
         A: [
             {
                 text: "You are going to be the leader of this quest and will be the creator of this website.
-                    
 Step 1: Create a repository on GitHub. Go to GitHub, click the '+' button near the search bar, and select 'New Repository.' Make it public, name it, add a README, then go to Collaborators & Teams. Click Add Collaborators and add the username of your team and give them the admin role.",
             },
             {
